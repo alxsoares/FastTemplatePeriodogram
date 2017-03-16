61cfdfd4404dcd55e23e0bf87bba74b4e81da2cc
Jake VanderPlas <jakevdp@uw.edu>
2017-03-16 06:53:42 -0700

    DOC: docstrings for slow template method

--- pyftp/slow_template_periodogram.py
+++ pyftp/slow_template_periodogram.py
@@ -7,7 +7,8 @@ class SlowTemplatePeriodogram(object):
     """Slow periodogram built from a template model.
 
     When computing the periodogram, this performs a nonlinear optimization at
-    each frequency.
+    each frequency. This is used mainly for testing the faster method
+    available in FastTemplatePeriodogram
 
     Parameters
     ----------
@@ -20,14 +21,26 @@ class SlowTemplatePeriodogram(object):
     template : Template object
         callable object that returns the template value as a function of phase
     """
-    def __init__(self, t, y, dy, template):
-        # Validate arrays
-        t, y, dy = np.broadcast_arrays(t, y, dy)
-        assert t.ndim == 1
-        self.t = t
-        self.y = y
-        self.dy = dy
+    def __init__(self, t, y, dy=None, template=None):
+        self.t, self.y, self.dy = self._validate_inputs(t, y, dy)
         self.template = template
+
+    def _validate_inputs(self, t, y, dy):
+        if dy is None:
+            # TODO: handle dy = None case more efficiently
+            t, y, dy = np.broadcast_arrays(t, y, 1.0)
+        else:
+            t, y, dy = np.broadcast_arrays(t, y, dy)
+        if t.ndim != 1:
+            raise ValueError("Inputs (t, y, dy) must be 1-dimensional")
+        return t, y, dy
+
+    def _chi2_ref(self):
+        """Compute the reference chi-square"""
+        weights = self.dy ** -2
+        weights /= weights.sum()
+        ymean = np.dot(weights, self.y)
+        return np.sum((self.y - ymean) ** 2 / self.dy ** 2)
 
     def _minimize_chi2_at_single_freq(self, freq):
         t_phase = (self.t * freq) % 1
@@ -36,17 +49,10 @@ class SlowTemplatePeriodogram(object):
             y_model = offset + amp * self.template(self.t * freq - phase)
             return np.sum((y_model - self.y) ** 2 / self.dy ** 2)
         params0 = [0, np.mean(self.y), np.std(self.y)]
-        res = optimize.minimize(chi2, params0, method='l-bfgs-b')
-        return res
-
-    def _chi2_ref(self):
-        weights = 1. / self.dy ** 2
-        weights /= weights.sum()
-        ymean = np.dot(weights, self.y)
-        return np.sum((self.y - ymean) ** 2 / self.dy ** 2)
+        return optimize.minimize(chi2, params0, method='l-bfgs-b')
 
     def power(self, freq):
-        """Compute a template-based periodogram
+        """Compute a template-based periodogram at the given frequencies
 
         Parameters
         ----------
@@ -59,12 +65,10 @@ class SlowTemplatePeriodogram(object):
             normalized power spectrum computed at the given frequencies
         """
         freq = np.asarray(freq)
-        shape = freq.shape
-        freq = freq.ravel()
-        results = list(map(self._minimize_chi2_at_single_freq, freq))
-        num_misses = sum([not res.success for res in results])
-        if num_misses:
+        results = list(map(self._minimize_chi2_at_single_freq, freq.flat))
+        failures = sum([not res.success for res in results])
+        if failures:
             warnings.warn("{0}/{1} frequency values failed to "
-                          "converge".format(num_misses, len(freq)))
+                          "converge".format(failures, len(freq)))
         chi2 = np.array([res.fun for res in results])
-        return (1 - chi2 / self._chi2_ref()).reshape(shape)
+        return np.reshape(1 - chi2 / self._chi2_ref(), freq.shape)
